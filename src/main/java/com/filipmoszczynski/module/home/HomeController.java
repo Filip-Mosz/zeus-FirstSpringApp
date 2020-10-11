7b56a185f86e29970cdbbf9de68e7e647f52e2f6
Filip M <filip.moszczynski@onet.pl>
2020-10-11 13:29:48 +0200

    >Dashboard module

--- src/main/java/com/filipmoszczynski/module/home/HomeController.java
+++ src/main/java/com/filipmoszczynski/module/home/HomeController.java
@@ -4,17 +4,33 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
     @RequestMapping("/")
     public String getHome(Model model) {
         PersonDTO person = new PersonDTO(
-                "Filip",
-                "Moszczyński",
+                "Hans",
+                "Talhoffer",
                 31
         );
+        PersonDTO person2 = new PersonDTO(
+                "Johannes",
+                "Lichtenauer",
+                32
+        );
+        PersonDTO person3 = new PersonDTO(
+                "Fiore",
+                "de Liberi",
+                33
+        );
 
+        List<PersonDTO> personsList = List.of(person,person2,person3);
+
+        model.addAttribute("name", "Filip");
         model.addAttribute("person", person);
+        model.addAttribute("personsList", personsList);
         return "home";
     }
 }
