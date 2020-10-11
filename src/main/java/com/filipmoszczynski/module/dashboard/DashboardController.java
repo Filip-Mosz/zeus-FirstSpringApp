704670ebd8798dfd9f01f850acf12e0b8b74948f
Filip M <filip.moszczynski@onet.pl>
2020-10-11 15:13:06 +0200

    >DI/@Service/v2 (preferred method)

--- src/main/java/com/filipmoszczynski/module/dashboard/DashboardController.java
+++ src/main/java/com/filipmoszczynski/module/dashboard/DashboardController.java
@@ -8,10 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DashboardController {
 
-    @Autowired
+
     private MenuService menuService;
-    @Autowired
     private ArticleService articleService;
+
+    @Autowired // opcjonalna; Spring wstrzyknąłby zalezności tak czy inaczej dzięki adnotacji @Service w odpowiednich klasach
+    public DashboardController(MenuService menuService, ArticleService articleService) {
+        this.menuService = menuService;
+        this.articleService = articleService;
+    }
 
     @RequestMapping("/dashboard")
     public String getDashboard (Model model) {
