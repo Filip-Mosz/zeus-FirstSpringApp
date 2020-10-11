c19f98f40944476caae26bef04bfa1a02f9b9cd6
Filip M <filip.moszczynski@onet.pl>
2020-10-11 11:13:38 +0200

    >Controller/View/Model

--- src/main/java/com/filipmoszczynski/module/home/HomeController.java
+++ src/main/java/com/filipmoszczynski/module/home/HomeController.java
@@ -1,12 +1,14 @@
 package com.filipmoszczynski.module.home;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String getHome() {
+    public String getHome(Model model) {
+        model.addAttribute("name", "Filip");
         return "home";
     }
 }
